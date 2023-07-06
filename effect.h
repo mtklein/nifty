6a5d1060d00da131ace76ad8ac928452389c233f
Mike Klein <mtklein@gmail.com>
2023-07-06 11:50:18 -0400

    simplify dropping none build

--- effect.h
+++ effect.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#if defined(__GNUC__) || defined(__clang__)
-    #define vector_size(sz) __attribute__((__vector_size__(sz)))
-#else
-    #define vector_size(sz)
-#endif
-
 #if defined(__ARM_FEATURE_FP16_VECTOR_ARITHMETIC)
     typedef _Float16 half;
 #else
@@ -13,14 +7,12 @@
 #endif
 
 #if defined(__AVX__)
-    typedef half vector_size(32) Half;
+    typedef half __attribute__((vector_size(32))) Half;
 #else
-    typedef half vector_size(16) Half;
+    typedef half __attribute__((vector_size(16))) Half;
 #endif
 
-typedef float vector_size(sizeof(Half)/sizeof(half) * sizeof(float)) Float;
-
-#undef vector_size
+typedef float __attribute__((vector_size(sizeof(Half)/sizeof(half) * sizeof(float)))) Float;
 
 extern struct Effect {
     void (*fn)(struct Effect const*,int, Half,Half,Half,Half, Half,Half,Half,Half);
--- expect.h
+++ expect.h
@@ -1,5 +1,4 @@
 #pragma once
 
-void abort(void);
-int  dprintf(int, char const*, ...);
-#define expect(x) if (!(x)) dprintf(2, "%s:%d expect(%s)\n", __FILE__,__LINE__,#x), abort()
+int dprintf(int, char const*, ...);
+#define expect(x) if (!(x)) dprintf(2, "%s:%d expect(%s)\n", __FILE__,__LINE__,#x), __builtin_trap()
